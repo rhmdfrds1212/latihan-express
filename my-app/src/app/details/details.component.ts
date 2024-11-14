efb9292be8d33bf65dd1e0a74be2ac58ed1d85ef
rahmad firdaus <163318781+rhmdfrds1212@users.noreply.github.com>
2024-11-14 14:27:34 +0700

    @Angular update

---
+++ my-app/src/app/details/details.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingLocation } from '../housing-location';
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      details works {{ housingLocationId }}
+    </p>
+  `,
+  styleUrl: './details.component.css'
+})
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = 0;
+
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id'])
+  }
+}
