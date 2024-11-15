78f9a6df9c834de9d84ac63581bbae386e04f23c
Alfin0431 <163318781+rhmdfrds1212@users.noreply.github.com>
2024-11-15 16:54:22 +0700

    @Angular update 2

--- my-app/src/app/housing-location/housing-location.component.ts
+++ my-app/src/app/housing-location/housing-location.component.ts
@@ -12,6 +12,7 @@ import { RouterModule } from '@angular/router';
       <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
       <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+      <p style="padding:0 0 10px 20px;">{{ housingLocation.availableUnits}} Unit(s)</p>
       <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
